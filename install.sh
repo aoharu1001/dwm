83b6342363584ae1ff4200bf692eedbb18bbe7e6
plana-planetarium <3412294524@qq.com>
2024-06-16 17:33:34 +0800

    update

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #! /usr/bin/bash
+echo "Create a new folder for dwm in ~/.dwm\n"
 mkdir ~/.dwm
+echo "Move files to ~/.dwm\n"
 cp -r ./* ~/.dwm
+echo "Change work directory\n"
 cd ~/.dwm
+echo "Make and install dwm\n"
 sudo make clean install
-mv xinitrc ~/.xinitrc
-sudo mv ./xinitrcsession-helper /usr/bin/xinitrcsession-helper
-sudo mv ./xinitrc.desktop /usr/share/xsessions/xinitrc.desktop
+echo "Move profile to ~/.xprofile\n"
+mv ./xprofile ~/.xprofile
+echo "Move dwm.desktop to /usr/share/xsessions/dwm.desktop\n"
+sudo mv ./dwm.desktop /usr/share/xsessions/dwm.desktop
+echo "Install finished\n"
