c8f916b7f55bfc25a3fde6ec057924957cc17544
plana-planetarium <3412294524@qq.com>
2024-06-16 15:05:48 +0800

    update

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ cp -r ./* ~/.dwm
 cd ~/.dwm
 sudo make clean install
 mv xinitrc ~/.xinitrc
+sudo mv ./xinitrcsession-helper /usr/bin/xinitrcsession-helper
+sudo mv ./xinitrc.desktop /usr/share/xsessions/xinitrc.desktop
